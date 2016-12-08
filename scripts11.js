5de76763a2073397a317194babafdf01c86d003c
vitcum <vitcum@gmail.com>
2016-12-08 10:03:20 +0100

    Add files via upload

---
+++ scripts11.js
@@ -0,0 +1,15 @@
+$(function(){
+	var carouselList = $("#carousel ul");
+	setInterval(changeSlide, 3000);
+	
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	}
+	
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
+});
